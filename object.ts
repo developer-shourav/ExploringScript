646890f5886b90881231e215ad1d903944308b07
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-02-16 23:23:29 +0600

    object type currections

---
+++ object.ts
@@ -0,0 +1,26 @@
+const city: string = "Dhaka" ;
+const fruit: string[] = ["kola", 'Am', 'jam', "kathal", 'lichu', "peyara", "Ata", "Begun"] ;
+
+const details: {personName: string, age: number, DOB: string, qulifications: string[]} = {
+    personName:"korm",
+    age:25,
+    DOB: "20th january",
+    qulifications: ["JSC", 'SSC', 'HSC', 'BSC']
+
+}
+
+type pupil= {
+    name: string,
+    agea: number,
+    marriedStatous: boolean ,
+    qulification: string[]
+
+}
+
+const student: pupil = {
+    name:"Fahim",
+    agea:254545,
+    marriedStatous: false,
+    qulification: ["ssc",'jsc']
+
+}
