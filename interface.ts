4208ff7f458a96b4133827f6416223697f0d8eba
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-02-17 00:02:56 +0600

    task list added

---
+++ interface.ts
@@ -0,0 +1,16 @@
+interface Player {
+    name: string,
+    club: string,
+    salary: number,
+    wife?: string | boolean
+}
+
+
+const messy: Player = {
+    name: 'L Messy',
+    club: 'Abahoni',
+    salary: 50000,
+    
+
+
+}
