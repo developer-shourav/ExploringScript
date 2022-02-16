efe3046c9595ef9169d5b4a4c2136fe0650e73fd
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-02-16 17:49:05 +0600

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+
+const myPhoneNumber =126242365;
+const myHome = "Dhaka,Bangladesh" ;
+const watchPrice = "25,000TK" ;
+
+const price1 = 250 ;
+const price2 = 500 ;
+const totalPrice = price1 + price2 ;
+console.log(myPhoneNumber, myHome,"MY Watch Price:", watchPrice);
+console.log("Total price of Shopping",totalPrice);
+
+
