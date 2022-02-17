5989a0b4f9b803d2a4cd8ed23b3fa6732d5288ae
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-02-17 20:50:09 +0600

    info added

--- class.ts
+++ class.ts
@@ -1,9 +1,34 @@
 const lalbaba: string = "Limpa luma tatu." ;
 
-function sum(num1: number , num2: number):number {
+function sum(num1: number , num2:number ):number {
 
     return num1 + num2 ;
 }
 
 
+/* Different types in TypeScript
 
+//Catagory One
+1.String
+2.Number
+3.Array
+4. null
+5.undefined
+6.bollean
+7.never
+8.unknown
+
+//Catagory Two
+any 
+
+
+
+*/
+
+const substract= ( number1: number, number2: number): number => {
+    return number1 - number2 ;
+}
+
+
+substract(20 , 10)
+console.log(substract);
